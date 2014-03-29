fddc1b2d4bd27129254c3022e44a3eb7a5718c48
igorcandeia <igor.morais@ccc.ufcg.edu.br>
2014-03-29 00:36:31 -0300

    Mechendo mais na interface

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -37,30 +37,32 @@ Seq[Any](format.raw/*1.35*/("""
 
 <body>
 """),_display_(Seq[Any](/*11.2*/main("Plano de Curso", user)/*11.30*/ {_display_(Seq[Any](format.raw/*11.32*/("""
-	<center><h2>"""),_display_(Seq[Any](/*12.15*/Messages("planoDeCurso"))),format.raw/*12.39*/("""</h2></center>
-
+	<div>
+	<center><h3>"""),_display_(Seq[Any](/*13.15*/Messages("planoDeCurso"))),format.raw/*13.39*/("""</h3></center>
+	<br/>
+	</div>
 
 <div class="periodo">
-		<form action=""""),_display_(Seq[Any](/*16.18*/routes/*16.24*/.Application.atualizaPeriodo)),format.raw/*16.52*/("""" method="post">
+		<form action=""""),_display_(Seq[Any](/*18.18*/routes/*18.24*/.Application.atualizaPeriodo)),format.raw/*18.52*/("""" method="post">
 			<ul style="list-style:none;">
             	<span style="float:left;margin-left:0px;font-size:20px;">Periodo Atual:</span>
-            	<input type="number" style="margin-bot:10px;height:25px;width:35px" min="1" max="10" name="periodo" title="Periodo entre 1 e 10" required value=""""),_display_(Seq[Any](/*19.160*/plano/*19.165*/.periodoAtual)),format.raw/*19.178*/("""" ><br/>
+            	<input type="number" style="margin-bot:10px;height:25px;width:35px" min="1" max="10" name="periodo" title="Periodo entre 1 e 10" required value=""""),_display_(Seq[Any](/*21.160*/plano/*21.165*/.periodoAtual)),format.raw/*21.178*/("""" ><br/>
                 <input type="submit" style="position:center;margin-top=10px;float:center;" title="Periodo entre 1 e 10"  value="Alterar">
 			</ul>
 		</form>
 			<div><br/>
            <span style="width:190px;height:30px;font-size:20px;margin-bottom:10px;margin-left:0px;"> Informacoes Uteis</span><br></br>
-                        			<strong><span class="periodoCreditos">Periodo Atual: """),_display_(Seq[Any](/*25.82*/plano/*25.87*/.getPeriodoAtual())),format.raw/*25.105*/("""</span></strong><br>
-			<span class="periodoCreditos">Creditos disciplinas nao alocadas: """),_display_(Seq[Any](/*26.70*/plano/*26.75*/.totalCreditosCadeirasNaoAlocadas())),format.raw/*26.110*/("""</span><br>
-			<span class="periodoCreditos">Creditos periodos passados: """),_display_(Seq[Any](/*27.63*/plano/*27.68*/.getCreditosPeriodosPassados())),format.raw/*27.98*/("""</span><br>
-			<span class="periodoCreditos">Creditos periodo atual: """),_display_(Seq[Any](/*28.59*/plano/*28.64*/.getCreditosPeriodoAtual())),format.raw/*28.90*/("""</span><br>
-			<span class="periodoCreditos">Creditos periodos Planejados: """),_display_(Seq[Any](/*29.65*/plano/*29.70*/.getCreditosPeriodosFuturos())),format.raw/*29.99*/("""</span><br>
-			<span class="periodoCreditos">Creditos minimos que faltam para me formar: """),_display_(Seq[Any](/*30.79*/plano/*30.84*/.getCreditosQueFaltamParaSeFormar())),format.raw/*30.119*/("""</span><br>
+                        			<strong><span class="periodoCreditos">Periodo Atual: """),_display_(Seq[Any](/*27.82*/plano/*27.87*/.getPeriodoAtual())),format.raw/*27.105*/("""</span></strong><br>
+			<span class="periodoCreditos">Creditos disciplinas nao alocadas: """),_display_(Seq[Any](/*28.70*/plano/*28.75*/.totalCreditosCadeirasNaoAlocadas())),format.raw/*28.110*/("""</span><br>
+			<span class="periodoCreditos">Creditos periodos passados: """),_display_(Seq[Any](/*29.63*/plano/*29.68*/.getCreditosPeriodosPassados())),format.raw/*29.98*/("""</span><br>
+			<span class="periodoCreditos">Creditos periodo atual: """),_display_(Seq[Any](/*30.59*/plano/*30.64*/.getCreditosPeriodoAtual())),format.raw/*30.90*/("""</span><br>
+			<span class="periodoCreditos">Creditos periodos Planejados: """),_display_(Seq[Any](/*31.65*/plano/*31.70*/.getCreditosPeriodosFuturos())),format.raw/*31.99*/("""</span><br>
+			<span class="periodoCreditos">Creditos minimos que faltam para me formar: """),_display_(Seq[Any](/*32.79*/plano/*32.84*/.getCreditosQueFaltamParaSeFormar())),format.raw/*32.119*/("""</span><br>
 
 			<strong><nome>Periodos abaixo do minimo de creditos:</nome></strong><br>
-			"""),_display_(Seq[Any](/*33.5*/for(periodo <- plano.periodosComMenosQueMinimoDeCreditos()) yield /*33.64*/{_display_(Seq[Any](format.raw/*33.65*/("""
-				<strong><span class="periodoCreditos"> """),_display_(Seq[Any](/*34.45*/periodo/*34.52*/.toString())),format.raw/*34.63*/("""</span></strong><br>
-			""")))})),format.raw/*35.5*/("""
+			"""),_display_(Seq[Any](/*35.5*/for(periodo <- plano.periodosComMenosQueMinimoDeCreditos()) yield /*35.64*/{_display_(Seq[Any](format.raw/*35.65*/("""
+				<strong><span class="periodoCreditos"> """),_display_(Seq[Any](/*36.45*/periodo/*36.52*/.toString())),format.raw/*36.63*/("""</span></strong><br>
+			""")))})),format.raw/*37.5*/("""
 			</div>
 		</div>
 
@@ -69,49 +71,49 @@ Seq[Any](format.raw/*1.35*/("""
 	<div class="periodo">
 	<span style="width:190px;height:30px;font-size:20px;margin-bottom:10px;margin-left:0px;"> Cadeiras disponiveis</span>
 	<ul style="list-style:none;"> 
-		"""),_display_(Seq[Any](/*44.4*/for(cadeira <- plano.getCadeiraDispniveisOrdenadas()) yield /*44.57*/ {_display_(Seq[Any](format.raw/*44.59*/("""
-		<div class="disponiveis" id=""""),_display_(Seq[Any](/*45.33*/cadeira/*45.40*/.getNome())),format.raw/*45.50*/(""""	draggable="true" 
+		"""),_display_(Seq[Any](/*46.4*/for(cadeira <- plano.getCadeiraDispniveisOrdenadas()) yield /*46.57*/ {_display_(Seq[Any](format.raw/*46.59*/("""
+		<div class="disponiveis" id=""""),_display_(Seq[Any](/*47.33*/cadeira/*47.40*/.getNome())),format.raw/*47.50*/(""""	draggable="true" 
 		ondragstart="drag(event)" ondragend="dragEnd()">
-	  		<center><label class="nome">"""),_display_(Seq[Any](/*47.35*/cadeira/*47.42*/.getNome())),format.raw/*47.52*/("""</label></center>
-		  	<label class="creditos">"""),_display_(Seq[Any](/*48.31*/cadeira/*48.38*/.getCreditos())),format.raw/*48.52*/(""" """),_display_(Seq[Any](/*48.54*/Messages("creditos"))),format.raw/*48.74*/("""</label>
-		  	<label class="dificuldade"> dificuldade: """),_display_(Seq[Any](/*49.48*/cadeira/*49.55*/.getDificuldade())),format.raw/*49.72*/("""</label>
+	  		<center><label class="nome">"""),_display_(Seq[Any](/*49.35*/cadeira/*49.42*/.getNome())),format.raw/*49.52*/("""</label></center>
+		  	<label class="creditos">"""),_display_(Seq[Any](/*50.31*/cadeira/*50.38*/.getCreditos())),format.raw/*50.52*/(""" """),_display_(Seq[Any](/*50.54*/Messages("creditos"))),format.raw/*50.74*/("""</label>
+		  	<label class="dificuldade"> dificuldade: """),_display_(Seq[Any](/*51.48*/cadeira/*51.55*/.getDificuldade())),format.raw/*51.72*/("""</label>
 	  	</div>
-	""")))})),format.raw/*51.3*/(""" 
+	""")))})),format.raw/*53.3*/(""" 
 
 	</ul>
 	<span class="periodoCreditos">Arraste para alocar</span>
 	</div>
 
 	<div>
-	"""),_display_(Seq[Any](/*58.3*/for(periodo <- plano.getPeriodos()) yield /*58.38*/ {_display_(Seq[Any](format.raw/*58.40*/("""
+	"""),_display_(Seq[Any](/*60.3*/for(periodo <- plano.getPeriodos()) yield /*60.38*/ {_display_(Seq[Any](format.raw/*60.40*/("""
 	
-		<div id=""""),_display_(Seq[Any](/*60.13*/periodo/*60.20*/.getNumero())),format.raw/*60.32*/("""" class="periodo" ondrop="drop(event, this)"
+		<div id=""""),_display_(Seq[Any](/*62.13*/periodo/*62.20*/.getNumero())),format.raw/*62.32*/("""" class="periodo" ondrop="drop(event, this)"
 			ondragover="allowDrop(event,this)" ondragleave="leave(event, this)">
 
 		<ul style="list-style:none;">
-			<span style="width:190px;height:30px;font-size:20px;margin-bottom:10px;margin-left:-40px;">"""),_display_(Seq[Any](/*64.96*/periodo/*64.103*/.getNumero())),format.raw/*64.115*/(""" """),_display_(Seq[Any](/*64.117*/Messages("nPeriodo"))),format.raw/*64.137*/(""" </span>
-			"""),_display_(Seq[Any](/*65.5*/for(cadeira <- periodo.getCadeiras()) yield /*65.42*/ {_display_(Seq[Any](format.raw/*65.44*/("""
-				"""),_display_(Seq[Any](/*66.6*/if(plano.isAlocadaNoLugarCorreto(cadeira, periodo))/*66.57*/ {_display_(Seq[Any](format.raw/*66.59*/("""
-		  		<div class="alocadas" id=""""),_display_(Seq[Any](/*67.34*/cadeira/*67.41*/.getNome())),format.raw/*67.51*/("""" draggable="true" 
-				ondragstart="drag(event)" ondragend="dragEnd()" onclick="remCadeira('"""),_display_(Seq[Any](/*68.75*/cadeira/*68.82*/.getNome())),format.raw/*68.92*/("""','true')">
-				""")))}/*69.7*/else/*69.12*/{_display_(Seq[Any](format.raw/*69.13*/("""
-				<div class="alocadas-errada" id=""""),_display_(Seq[Any](/*70.39*/cadeira/*70.46*/.getNome())),format.raw/*70.56*/("""" draggable="true" 
-				ondragstart="drag(event)" ondragend="dragEnd()" onclick="remCadeira('"""),_display_(Seq[Any](/*71.75*/cadeira/*71.82*/.getNome())),format.raw/*71.92*/("""','true')">
-				""")))})),format.raw/*72.6*/("""
-		  		<center> <span>"""),_display_(Seq[Any](/*73.23*/cadeira/*73.30*/.getNome())),format.raw/*73.40*/("""</span></center>
-		  		<label class="creditos">"""),_display_(Seq[Any](/*74.32*/cadeira/*74.39*/.getCreditos())),format.raw/*74.53*/(""" """),_display_(Seq[Any](/*74.55*/Messages("creditos"))),format.raw/*74.75*/("""</label>
-		  		<label class="dificuldade"> dificuldade: """),_display_(Seq[Any](/*75.49*/cadeira/*75.56*/.getDificuldade())),format.raw/*75.73*/("""</label>
+			<span style="width:190px;height:30px;font-size:20px;margin-bottom:10px;margin-left:-40px;">"""),_display_(Seq[Any](/*66.96*/periodo/*66.103*/.getNumero())),format.raw/*66.115*/(""" """),_display_(Seq[Any](/*66.117*/Messages("nPeriodo"))),format.raw/*66.137*/(""" </span>
+			"""),_display_(Seq[Any](/*67.5*/for(cadeira <- periodo.getCadeiras()) yield /*67.42*/ {_display_(Seq[Any](format.raw/*67.44*/("""
+				"""),_display_(Seq[Any](/*68.6*/if(plano.isAlocadaNoLugarCorreto(cadeira, periodo))/*68.57*/ {_display_(Seq[Any](format.raw/*68.59*/("""
+		  		<div class="alocadas" id=""""),_display_(Seq[Any](/*69.34*/cadeira/*69.41*/.getNome())),format.raw/*69.51*/("""" draggable="true" 
+				ondragstart="drag(event)" ondragend="dragEnd()" onclick="remCadeira('"""),_display_(Seq[Any](/*70.75*/cadeira/*70.82*/.getNome())),format.raw/*70.92*/("""','true')">
+				""")))}/*71.7*/else/*71.12*/{_display_(Seq[Any](format.raw/*71.13*/("""
+				<div class="alocadas-errada" id=""""),_display_(Seq[Any](/*72.39*/cadeira/*72.46*/.getNome())),format.raw/*72.56*/("""" draggable="true" 
+				ondragstart="drag(event)" ondragend="dragEnd()" onclick="remCadeira('"""),_display_(Seq[Any](/*73.75*/cadeira/*73.82*/.getNome())),format.raw/*73.92*/("""','true')">
+				""")))})),format.raw/*74.6*/("""
+		  		<center> <span>"""),_display_(Seq[Any](/*75.23*/cadeira/*75.30*/.getNome())),format.raw/*75.40*/("""</span></center>
+		  		<label class="creditos">"""),_display_(Seq[Any](/*76.32*/cadeira/*76.39*/.getCreditos())),format.raw/*76.53*/(""" """),_display_(Seq[Any](/*76.55*/Messages("creditos"))),format.raw/*76.75*/("""</label>
+		  		<label class="dificuldade"> Dificuldade: """),_display_(Seq[Any](/*77.49*/cadeira/*77.56*/.getDificuldade())),format.raw/*77.73*/("""</label>
 		  	</div>
 
-		""")))})),format.raw/*78.4*/(""" 
+		""")))})),format.raw/*80.4*/(""" 
 		
 		</ul>
-		<span class="periodoCreditos">"""),_display_(Seq[Any](/*81.34*/periodo/*81.41*/.getCreditos())),format.raw/*81.55*/(""" """),_display_(Seq[Any](/*81.57*/Messages("creditos"))),format.raw/*81.77*/("""</span>
-		<span class="periodoDificuldade">Dificuldade: """),_display_(Seq[Any](/*82.50*/periodo/*82.57*/.getDificuldadeTotal())),format.raw/*82.79*/("""</span>
+		<span class="periodoCreditos">"""),_display_(Seq[Any](/*83.34*/periodo/*83.41*/.getCreditos())),format.raw/*83.55*/(""" """),_display_(Seq[Any](/*83.57*/Messages("creditos"))),format.raw/*83.77*/("""</span>
+		<span class="periodoDificuldade">Dificuldade: """),_display_(Seq[Any](/*84.50*/periodo/*84.57*/.getDificuldadeTotal())),format.raw/*84.79*/("""</span>
 		</div>
-	""")))})),format.raw/*84.3*/("""
+	""")))})),format.raw/*86.3*/("""
 	</div>
-""")))})),format.raw/*86.2*/("""
+""")))})),format.raw/*88.2*/("""
 </body>"""))}
     }
     
@@ -124,11 +126,11 @@ Seq[Any](format.raw/*1.35*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri Mar 28 04:15:37 BRT 2014
+                    DATE: Fri Mar 28 16:54:14 BRT 2014
                     SOURCE: C:/Users/Igor/si1/PSI1/app/views/index.scala.html
-                    HASH: ba8845c8274f0828019215f6efae7c50b1a4b823
-                    MATRIX: 785->1|929->34|959->56|1097->159|1111->165|1168->201|1246->244|1260->250|1315->284|1461->395|1498->423|1538->425|1590->441|1636->465|1732->525|1747->531|1797->559|2138->863|2153->868|2189->881|2627->1283|2641->1288|2682->1306|2809->1397|2823->1402|2881->1437|2992->1512|3006->1517|3058->1547|3165->1618|3179->1623|3227->1649|3340->1726|3354->1731|3405->1760|3532->1851|3546->1856|3604->1891|3735->1987|3810->2046|3849->2047|3931->2093|3947->2100|3980->2111|4037->2137|4281->2346|4350->2399|4390->2401|4460->2435|4476->2442|4508->2452|4651->2559|4667->2566|4699->2576|4784->2625|4800->2632|4836->2646|4874->2648|4916->2668|5009->2725|5025->2732|5064->2749|5119->2773|5247->2866|5298->2901|5338->2903|5391->2920|5407->2927|5441->2939|5726->3188|5743->3195|5778->3207|5817->3209|5860->3229|5909->3243|5962->3280|6002->3282|6044->3289|6104->3340|6144->3342|6215->3377|6231->3384|6263->3394|6394->3489|6410->3496|6442->3506|6478->3525|6491->3530|6530->3531|6606->3571|6622->3578|6654->3588|6785->3683|6801->3690|6833->3700|6882->3718|6942->3742|6958->3749|6990->3759|7075->3808|7091->3815|7127->3829|7165->3831|7207->3851|7301->3909|7317->3916|7356->3933|7415->3961|7500->4010|7516->4017|7552->4031|7590->4033|7632->4053|7726->4111|7742->4118|7786->4140|7838->4161|7881->4173
-                    LINES: 26->1|30->1|32->4|34->6|34->6|34->6|35->7|35->7|35->7|39->11|39->11|39->11|40->12|40->12|44->16|44->16|44->16|47->19|47->19|47->19|53->25|53->25|53->25|54->26|54->26|54->26|55->27|55->27|55->27|56->28|56->28|56->28|57->29|57->29|57->29|58->30|58->30|58->30|61->33|61->33|61->33|62->34|62->34|62->34|63->35|72->44|72->44|72->44|73->45|73->45|73->45|75->47|75->47|75->47|76->48|76->48|76->48|76->48|76->48|77->49|77->49|77->49|79->51|86->58|86->58|86->58|88->60|88->60|88->60|92->64|92->64|92->64|92->64|92->64|93->65|93->65|93->65|94->66|94->66|94->66|95->67|95->67|95->67|96->68|96->68|96->68|97->69|97->69|97->69|98->70|98->70|98->70|99->71|99->71|99->71|100->72|101->73|101->73|101->73|102->74|102->74|102->74|102->74|102->74|103->75|103->75|103->75|106->78|109->81|109->81|109->81|109->81|109->81|110->82|110->82|110->82|112->84|114->86
+                    HASH: 7810e421562f91ea061455ac30224a3c3b9e9bd4
+                    MATRIX: 785->1|929->34|959->56|1097->159|1111->165|1168->201|1246->244|1260->250|1315->284|1461->395|1498->423|1538->425|1598->449|1644->473|1755->548|1770->554|1820->582|2161->886|2176->891|2212->904|2650->1306|2664->1311|2705->1329|2832->1420|2846->1425|2904->1460|3015->1535|3029->1540|3081->1570|3188->1641|3202->1646|3250->1672|3363->1749|3377->1754|3428->1783|3555->1874|3569->1879|3627->1914|3758->2010|3833->2069|3872->2070|3954->2116|3970->2123|4003->2134|4060->2160|4304->2369|4373->2422|4413->2424|4483->2458|4499->2465|4531->2475|4674->2582|4690->2589|4722->2599|4807->2648|4823->2655|4859->2669|4897->2671|4939->2691|5032->2748|5048->2755|5087->2772|5142->2796|5270->2889|5321->2924|5361->2926|5414->2943|5430->2950|5464->2962|5749->3211|5766->3218|5801->3230|5840->3232|5883->3252|5932->3266|5985->3303|6025->3305|6067->3312|6127->3363|6167->3365|6238->3400|6254->3407|6286->3417|6417->3512|6433->3519|6465->3529|6501->3548|6514->3553|6553->3554|6629->3594|6645->3601|6677->3611|6808->3706|6824->3713|6856->3723|6905->3741|6965->3765|6981->3772|7013->3782|7098->3831|7114->3838|7150->3852|7188->3854|7230->3874|7324->3932|7340->3939|7379->3956|7438->3984|7523->4033|7539->4040|7575->4054|7613->4056|7655->4076|7749->4134|7765->4141|7809->4163|7861->4184|7904->4196
+                    LINES: 26->1|30->1|32->4|34->6|34->6|34->6|35->7|35->7|35->7|39->11|39->11|39->11|41->13|41->13|46->18|46->18|46->18|49->21|49->21|49->21|55->27|55->27|55->27|56->28|56->28|56->28|57->29|57->29|57->29|58->30|58->30|58->30|59->31|59->31|59->31|60->32|60->32|60->32|63->35|63->35|63->35|64->36|64->36|64->36|65->37|74->46|74->46|74->46|75->47|75->47|75->47|77->49|77->49|77->49|78->50|78->50|78->50|78->50|78->50|79->51|79->51|79->51|81->53|88->60|88->60|88->60|90->62|90->62|90->62|94->66|94->66|94->66|94->66|94->66|95->67|95->67|95->67|96->68|96->68|96->68|97->69|97->69|97->69|98->70|98->70|98->70|99->71|99->71|99->71|100->72|100->72|100->72|101->73|101->73|101->73|102->74|103->75|103->75|103->75|104->76|104->76|104->76|104->76|104->76|105->77|105->77|105->77|108->80|111->83|111->83|111->83|111->83|111->83|112->84|112->84|112->84|114->86|116->88
                     -- GENERATED --
                 */
             
--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -31,34 +31,32 @@ Seq[Any](format.raw/*1.44*/("""
 <!DOCTYPE html>
 
 <html>
-    <head>
+	<head>
         <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
-        <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
-        <script src=""""),_display_(Seq[Any](/*10.23*/routes/*10.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*10.74*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(Seq[Any](/*8.23*/routes/*8.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*8.74*/("""" type="text/javascript"></script>
         
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*12.54*/routes/*12.60*/.Assets.at("stylesheets/bootstrap.css"))),format.raw/*12.99*/("""">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*13.54*/routes/*13.60*/.Assets.at("stylesheets/bootstrap.min.css"))),format.raw/*13.103*/("""">
-       	<link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*14.54*/routes/*14.60*/.Assets.at("stylesheets/bootstrap-theme.min.css"))),format.raw/*14.109*/("""">
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*15.54*/routes/*15.60*/.Assets.at("stylesheets/bootstrap-theme.css"))),format.raw/*15.105*/("""">
-        
-        <script src=""""),_display_(Seq[Any](/*17.23*/routes/*17.29*/.Assets.at("javascripts/bootstrap.min.js"))),format.raw/*17.71*/("""" type="text/javascript"></script>
-        <script src=""""),_display_(Seq[Any](/*18.23*/routes/*18.29*/.Assets.at("javascripts/bootstrap.js"))),format.raw/*18.67*/("""" type="text/javascript"></script>
-    </head>
+        <!-- CSS -->
+ 
+	    <link rel="stylesheet" href=""""),_display_(Seq[Any](/*12.36*/routes/*12.42*/.Assets.at("stylesheets/reset.css"))),format.raw/*12.77*/("""">
+	    <link rel="stylesheet" href=""""),_display_(Seq[Any](/*13.36*/routes/*13.42*/.Assets.at("stylesheets/animate.css"))),format.raw/*13.79*/("""">
+	    <link rel="stylesheet" href=""""),_display_(Seq[Any](/*14.36*/routes/*14.42*/.Assets.at("stylesheets/main.css"))),format.raw/*14.76*/("""">
+	     
+	     <!-- Basics -->
+ 
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	</head>        
     <body>
         
         
-        
- <head>   
-    <dl id="user">
-        <dt>"""),_display_(Seq[Any](/*26.14*/user/*26.18*/.getEmail())),format.raw/*26.29*/("""</dt>
+    <dl id="user" style="background:#27408B">
+        <dt style="color:#ffffff">Bem vindo, """),_display_(Seq[Any](/*25.47*/user/*25.51*/.getEmail())),format.raw/*25.62*/("""</dt>
         <dd>
-            <a href=""""),_display_(Seq[Any](/*28.23*/routes/*28.29*/.Application.logout())),format.raw/*28.50*/("""">Logout</a>
+            <a href=""""),_display_(Seq[Any](/*27.23*/routes/*27.29*/.Application.logout())),format.raw/*27.50*/("""">Logout</a>
         </dd>
     </dl>
     
-    </head>
-        """),_display_(Seq[Any](/*33.10*/content)),format.raw/*33.17*/("""
+        """),_display_(Seq[Any](/*31.10*/content)),format.raw/*31.17*/("""
         
         
         
@@ -77,11 +75,11 @@ Seq[Any](format.raw/*1.44*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Fri Mar 28 14:52:14 BRT 2014
+                    DATE: Fri Mar 28 16:40:46 BRT 2014
                     SOURCE: C:/Users/Igor/si1/PSI1/app/views/main.scala.html
-                    HASH: 5775804aa619c9c2dff5ea15cad305bf8708e8e9
-                    MATRIX: 783->1|919->43|1013->102|1039->107|1137->170|1151->176|1206->210|1303->272|1317->278|1370->310|1432->336|1447->342|1514->387|1649->486|1664->492|1725->531|1818->588|1833->594|1899->637|1992->694|2007->700|2079->749|2172->806|2187->812|2255->857|2327->893|2342->899|2406->941|2500->999|2515->1005|2575->1043|2747->1179|2760->1183|2793->1194|2872->1237|2887->1243|2930->1264|3034->1332|3063->1339
-                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|43->15|43->15|43->15|45->17|45->17|45->17|46->18|46->18|46->18|54->26|54->26|54->26|56->28|56->28|56->28|61->33|61->33
+                    HASH: 98ae879d8130e70985e90b69eacd96cf913ed146
+                    MATRIX: 783->1|919->43|1010->99|1036->104|1103->136|1117->142|1183->187|1325->293|1340->299|1397->334|1472->373|1487->379|1546->416|1621->455|1636->461|1692->495|2007->774|2020->778|2053->789|2132->832|2147->838|2190->859|2281->914|2310->921
+                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|40->12|40->12|40->12|41->13|41->13|41->13|42->14|42->14|42->14|53->25|53->25|53->25|55->27|55->27|55->27|59->31|59->31
                     -- GENERATED --
                 */
             
